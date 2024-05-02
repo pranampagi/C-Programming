78f7e8885c5a96721bab90342b8e8aa48be8d97c
Pranam Pagi <pranampagi@gmail.com>
2024-05-02 12:17:08 +0530

    Data Types

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -3,14 +3,10 @@
 
 int main()
 {
-    char characterName[] = "John";
-    int characterAge = 35;
-    printf("There once was a man named %s\n", characterName);
-    printf("he was %d years old.\n", characterAge);
-
-    characterAge = 30;
-    printf("He really liked the name  %s\n", characterName);
-    printf("but did not like being %d.\n", characterAge);
+    int age = 40;
+    double gpa = 3.6;
+    char grade = 'A';
+    char phrase[] = "Giraffe Academy";
 
     return 0;
 }
