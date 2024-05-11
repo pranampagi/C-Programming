bb59d407c6353ad5adb34d27a03cbba877113300
Pranam Pagi <pranampagi@gmail.com>
2024-05-11 12:23:36 +0530

    Arrays

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -3,21 +3,9 @@
 
 int main()
 {
-    char color[20];
-    char pluralNoun[20];
-    char celebrityF[20];
-    char celebrityL[20];
-
-    printf("Enter a color: ");
-    scanf("%s", color);
-    printf("Enter a plural noun: ");
-    scanf("%s", pluralNoun);
-    printf("Enter a celebrity: ");
-    scanf("%s%s", celebrityF, celebrityL);
-
-    printf("Roses are %s\n", color);
-    printf("%s are blue\n", pluralNoun);
-    printf("I love %s %s\n", celebrityF, celebrityL);
+    int luckyNumbers[10];
+    luckyNumbers[1] = 200;
+    printf("%d", luckyNumbers[1]);
 
     return 0;
 }
