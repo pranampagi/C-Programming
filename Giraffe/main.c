711465265d45d3ccd3d359de0d5e8839ca990317
Pranam Pagi <pranampagi@gmail.com>
2024-05-14 18:08:30 +0530

    While Loops

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Student {
-    char name[50];
-    char major[50];
-    int age;
-    double gpa;
-};
-
 int main()
 {
-    struct Student student1;
-    student1.age = 22;
-    student1.gpa = 3.2;
-    strcpy(student1.name, "Jim");
-    strcpy(student1.major, "Business");
+    /**
+    int index = 1;
 
-    struct Student student2;
-    student2.age = 20;
-    student2.gpa = 2.5;
-    strcpy(student2.name, "Pam");
-    strcpy(student2.major, "Art");
+    while (index <= 5) {
+        printf("%d\n", index);
+        index++;
+    }
+    */
 
-    printf("%s", student2.name);
+    int index = 6;
+
+    do {
+        printf("%d\n", index);
+        index++;
+    } while (index <= 5);
 
     return 0;
 }
