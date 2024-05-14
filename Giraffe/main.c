f65ef4e85fcc08d8dabc3648e8be8d3301b5e8dd
Pranam Pagi <pranampagi@gmail.com>
2024-05-14 20:50:29 +0530

    Pointers

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -4,10 +4,13 @@
 int main()
 {
     int age = 30;
+    int * pAge = &age;
     double gpa = 3.4;
+    double * pGpa = &gpa;
     char grade = 'A';
+    char * pGrade = &grade;
 
-    printf("age: %p\ngpa: %p\ngrade: %p", &age, &gpa, &grade);
+    printf("age's memory address: %p\n", &age);
 
     return 0;
 }
