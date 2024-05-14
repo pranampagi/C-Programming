820fd1a9bd9fc31d1413534c8e95bc2e9d737c26
Pranam Pagi <pranampagi@gmail.com>
2024-05-14 21:00:03 +0530

    Dereferencing Pointers

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -4,13 +4,10 @@
 int main()
 {
     int age = 30;
-    int * pAge = &age;
-    double gpa = 3.4;
-    double * pGpa = &gpa;
-    char grade = 'A';
-    char * pGrade = &grade;
+    int *pAge = &age;
 
-    printf("age's memory address: %p\n", &age);
+    // printf("%d", *pAge);
+    printf("%d", *&age);
 
     return 0;
 }
